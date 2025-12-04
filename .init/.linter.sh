01cd265bc6ddfa0ca146bf64eaab7119754d2328
Kavia AI <production@kavia.ai>
2025-12-04 09:31:12 +0000

    Checkpoint 1 - cg470f65e3

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/memory-match-game-183688-183697/memory_game_frontend
+flutter analyze
+
---
+++ memory_game_frontend/test/widget_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:memory_game_frontend/main.dart';
+
+void main() {
+  testWidgets('App generation message displayed', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.text('memory_game_frontend App is being generated...'), findsOneWidget);
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('App bar has correct title', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.text('memory_game_frontend'), findsOneWidget);
+  });
+}
